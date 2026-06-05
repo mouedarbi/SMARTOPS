395e567916819de700154040a62c126511b25d21
Mohamed Ouedarbi <mouedarbi@gmail.com>
2026-06-05 13:11:17 +0200

    Chore: Finalize scheduler vendoring and restore Django 6.0.4 with green CI

--- licensing/views.py
+++ licensing/views.py
@@ -17,7 +17,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, JsonResponse
 from django.core.management import call_command
 
 from .services import LicenseService
